0718a93559115496ed653dbe48dccc64c98853d1
cobragh <lucas_alvesf09@outlook.com>
2023-08-01 23:13:19 -0300

    Digital working

--- clock/script.js
+++ clock/script.js
@@ -1,3 +1,15 @@
-document.addEventListener("keydown", (event) => {
-  console.log(event);
-});
+let digital = document.querySelector(".digital");
+let s = document.querySelector(".p_s");
+let m = document.querySelector(".p_m");
+let h = document.querySelector(".p_h");
+
+updateClock = () => {
+  let now = new Date();
+  let getSeconds = now.getSeconds();
+  let getMinutes = now.getMinutes();
+  let getHours = now.getHours();
+
+  digital.innerHTML = `${getHours.toString()}: ${getMinutes.toString()}: ${getSeconds.toString()}`;
+};
+
+setInterval(updateClock, 1000);
